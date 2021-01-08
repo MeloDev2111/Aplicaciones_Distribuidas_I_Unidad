5f12e1e572217883fb29dac12eed818e214ffae4
MeloDev <melodiaz2111@gmail.com>
2021-01-07 20:54:26 -0500

    Version 1.1 , se le agregaron los links de los repos y del deploy

--- docs/Includes/footer.php
+++ docs/Includes/footer.php
@@ -1,4 +1,24 @@
 <footer>
+    <div class="bg-dark">
+        <h3 class="text-center text-white mb-3" >LINKS</h3>
+        <div class="row text-center pb-3">
+            <a href="https://github.com/MeloDev2111/" class="col" target="_blank" >
+                <img src="../docs/images/GitHub-Mark-Light-64px.png" title="Repositorio GitHub - repo privado"
+                alt="GitHub icon" height= 60px>
+            </a>
+
+            <a href="https://appsdistribuidas-i-unidad.herokuapp.com/" class="col" target="_blank">
+                <img src="../docs/images/heroku_icon.png" title="Deploy con Heroku" 
+                alt="Heroku icon" height= 60px>
+            </a>
+
+            <a href="https://unsanta-my.sharepoint.com/:f:/g/personal/201814005_uns_edu_pe/EtkcIN2kws5HhedCo7hOYW4BUfICy2P2zzzf2ZTZlio8Fg?e=5lfmpv" 
+            class="col" target="_blank">
+                <img src="../docs/images/microsoft_onedrive_icon.png" title="Repositorio OneDrive" 
+                alt="One Drive icon" height= 60px>
+            </a>
+        </div>
+    </div>
     <!----scripts---->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
